8636fbd0ed4a587e29ee660991bbffc40eee4ef2
Ben Leimberger <wbleimberger@gmail.com>
2022-01-23 09:26:04 -0500

    test

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const cache = new InMemoryCache();
 const client = new ApolloClient({
     // Provide required constructor fields
     cache: cache,
-    uri: 'http://localhost:4000/',
+    uri: process.env.UNI_V3_URL,
   
     // Provide some optional constructor fields
-    name: 'react-web-client',
-    version: '1.3',
+    name: 'pathfinder',
+    version: '1.0',
     queryDeduplication: false,
     defaultOptions: {
       watchQuery: {
@@ -17,3 +17,5 @@ const client = new ApolloClient({
       },
     },
   });
+
+  
